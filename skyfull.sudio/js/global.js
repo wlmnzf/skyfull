3ac52b9b8dc9fa1d355599fef442728117a34666
William Wang <wlmnzf@126.com>
2016-04-06 07:17:59 +0800

    no message

---
+++ skyfull.sudio/js/global.js
@@ -0,0 +1,38 @@
+function getstyle(obj,name){
+  if(obj.currentStyle){
+      return obj.currentStyle[name];
+    }else{
+      return getComputedStyle(obj,false)[name];
+    }
+}
+
+function startrun(obj,attr,target,fn){
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function(){
+      var cur = 0;
+        if(attr == "opacity"){
+          cur = Math.round(parseFloat(getstyle(obj,attr))*100);
+        }else{
+          cur = parseInt(getstyle(obj,attr));
+        }
+      var speed = (target-cur)/8;
+        speed = speed>0?Math.ceil(speed):Math.floor(speed);
+        
+        if(cur == target){
+          clearInterval(obj.timer);
+            if(fn){
+              fn();
+            }
+        }else{
+          if(attr == "opacity"){
+                obj.style.filter = "alpha(opacity="+(cur+speed)+")";
+              obj.style.opacity = (cur+speed)/100;
+            }else{
+            obj.style[attr] = cur + speed + "px";
+            }
+        }
+        
+    },20)
+}
+
+
---
+++ skyfull.sudio/js/project.js
@@ -0,0 +1,19 @@
+function ShowModal()
+{
+	var body=document.getElementsByTagName("body");
+	body.style.overflowY="hidden";
+	
+	var shade= document.getElementById("shade");
+	shade.add
+	shade.addEventListener("click",,false)
+}
+
+window.onload
+=function()
+{
+    var items=document.getElementsByName("item"); 
+    for(int i=0;i<items.length;i++)
+    {
+    	items[i].addEventListener("click",ShowModal,false)
+    }
+}
